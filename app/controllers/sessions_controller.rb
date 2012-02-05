a01082e15b55f1da7e766759c08159f2c4f21e6a
daigo <dragonmind2002@yahoo.co.jp>
2012-02-05 18:39:38 +0900

    不要処理を削除 #公開実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,11 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to "/", :notice => "ログインに成功しました！"
+    redirect_to "/"
   end
 
   def destroy
     reset_session
-    redirect_to "/", :notice => "ログアウトしました！"
+    redirect_to "/"
   end
 end
