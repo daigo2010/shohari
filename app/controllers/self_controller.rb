287f93d6e369a270fb83c86de50e338ef8e295f2
daigo <dragonmind2002@yahoo.co.jp>
2012-02-10 08:14:02 +0900

    自己評価登録処理を追加 #公開実装

--- app/controllers/self_controller.rb
+++ app/controllers/self_controller.rb
@@ -1,9 +1,21 @@
 class SelfController < ApplicationController
     def check 
-        @quest = Question.where("location = :location", :location => "jp")    
+        @quest = Question.where("location = :location", :location => "jp")
     end
 
     def post
-        @answer = Answer.new
+        Answer.destroy_all(['answer_user_id = :user_id AND target_user_id = :user_id', {:user_id => session[:user_id]}])
+        @quest = Question.where("location = :location", :location => "jp")
+        @quest.each do |q|
+            key = q.id.to_s
+            if params["question_" + q.id.to_s] then
+                @answer = Answer.new
+                @answer.answer_user_id = session[:user_id]
+                @answer.target_user_id = session[:user_id]
+                @answer.question_id    = q.id
+                @answer.save
+            end
+        end
+        redirect_to :controller => 'top'
     end
 end
