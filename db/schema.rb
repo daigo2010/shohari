34c58107b61322a71866decf16e6700df3b10fbb
daigo suganuma <dragonmind2002@yahoo.co.jp>
2012-02-11 15:40:32 +0900

    スキーマを微修正 #公開実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120207151233) do
+ActiveRecord::Schema.define(:version => 20120207143910) do
 
   create_table "answers", :force => true do |t|
     t.integer  "answer_user_id"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20120207151233) do
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
   end
+
   add_index "answers", ["answer_user_id"], :name => "index_answers_on_answer_user_id"
   add_index "answers", ["target_user_id"], :name => "index_answers_on_target_user_id"
 
