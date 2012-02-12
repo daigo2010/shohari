83b8df0f853a55d88b2d3ebd3ecb79c76bd9dbee
daigo <dragonmind2002@yahoo.co.jp>
2012-02-12 12:49:40 +0900

    #公開実装 他人評価機能追加＆登録機能入力部分を共通化

--- app/controllers/other_controller.rb
+++ app/controllers/other_controller.rb
@@ -16,25 +16,26 @@ class OtherController < ApplicationController
             res = User.where('uid = :uid', :uid => f['id'])
             next if !res
             res.each do |r|
-                @friends.push({:uid => r.uid, :name => r.name})
+                @friends.push({:user_id => r.id, :name => r.name})
             end
         end
     end
 
     def check 
         @quest = Question.where("location = :location", :location => "jp")
-        @answer = Answer.where("answer_user_id = :user_id AND target_user_id = :user_id", :user_id => session[:user_id])
+        @answer = Answer.where("answer_user_id = :answer_user_id AND target_user_id = :target_user_id", :answer_user_id => session[:user_id], :target_user_id => params[:user_id])
+        @user_id = params[:user_id]
     end
 
     def post
-        Answer.destroy_all(['answer_user_id = :user_id AND target_user_id = :user_id', {:user_id => session[:user_id]}])
+        Answer.destroy_all(['answer_user_id = :answer_user_id AND target_user_id = :target_user_id', {:answer_user_id => session[:user_id], :target_user_id => params[:user_id]}])
         @quest = Question.where("location = :location", :location => "jp")
         @quest.each do |q|
             key = q.id.to_s
             if params["question_" + q.id.to_s] then
                 @answer = Answer.new
                 @answer.answer_user_id = session[:user_id]
-                @answer.target_user_id = session[:user_id]
+                @answer.target_user_id = params[:user_id]
                 @answer.question_id    = q.id
                 @answer.save
             end
