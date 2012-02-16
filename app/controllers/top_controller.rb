e6453d2762f667205893b2c2640dd86ddc68ed10
daigo <dragonmind2002@yahoo.co.jp>
2012-02-16 23:17:35 +0900

    #公開実装 バリデート処理追加

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -4,16 +4,6 @@ class TopController < ApplicationController
             @done_self_check = true if Answer.where('answer_user_id = :user_id AND target_user_id = :user_id', :user_id => session[:user_id]).count > 0
             @done_other_check = true if Answer.where('answer_user_id = :user_id AND target_user_id <> :user_id', :user_id => session[:user_id]).count > 0
             @exists_my_shohari = true if Answer.where('answer_user_id <> :user_id AND target_user_id = :user_id', :user_id => session[:user_id]).count > 0
-###            require 'net/https'
-###            require 'uri'
-###
-###            friend_list_uri = Shohari::Application.config.fb_friend_uri
-###            uri = URI.parse(friend_list_uri + session[:access_token])
-###
-###            http = Net::HTTP.new(uri.host, uri.port)
-###            http.use_ssl = true
-###            http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-###            response = http.request(Net::HTTP::Get.new(uri.request_uri))
         else
             redirect_to '/auth/facebook'
         end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,6 @@
 class Answer < ActiveRecord::Base
     belongs_to :question
+    validates :answer_user_id, :target_user_id, :question_id,
+        :presence => true,
+        :numericality => {:only_integer => true}
 end
