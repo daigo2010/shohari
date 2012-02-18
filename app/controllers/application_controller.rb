d7abba174d3a979870b62f2abb257a9c0e757744
daigo <dragonmind2002@yahoo.co.jp>
2012-02-18 15:50:09 +0900

    #公開実装 セッションチェックと友達リスト取得処理を統合し簡素化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,32 @@ class ApplicationController < ActionController::Base
     protect_from_forgery
 
     helper_method :current_user
+    before_filter :session_check, :except => 'create'
 
     private
     def current_user
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+    def session_check
+        if session[:access_token] then
+            require 'net/https'
+            require 'uri'
+            friend_list_uri = Shohari::Application.config.fb_friend_uri
+            uri = URI.parse(friend_list_uri + session[:access_token])
+            http = Net::HTTP.new(uri.host, uri.port)
+            http.use_ssl = true
+            http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+            response = http.request(Net::HTTP::Get.new(uri.request_uri))
+            if response.code != "200" then
+                reset_session
+                redirect_to '/auth/facebook'
+                return
+            end
+            @friend_list = response.body
+        else
+            reset_session
+            redirect_to '/auth/facebook'
+            return
+        end
     end
 end
--- app/controllers/other_controller.rb
+++ app/controllers/other_controller.rb
@@ -1,22 +1,22 @@
 class OtherController < ApplicationController
     def list
-        require 'net/https'
-        require 'uri'
-        friend_list_uri = Shohari::Application.config.fb_friend_uri
-        uri = URI.parse(friend_list_uri + session[:access_token])
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        response = http.request(Net::HTTP::Get.new(uri.request_uri))
-
-        if response.code != "200" then
-            reset_session
-            redirect_to '/auth/facebook'
-            return
-        end
+#        require 'net/https'
+#        require 'uri'
+#        friend_list_uri = Shohari::Application.config.fb_friend_uri
+#        uri = URI.parse(friend_list_uri + session[:access_token])
+#        http = Net::HTTP.new(uri.host, uri.port)
+#        http.use_ssl = true
+#        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+#        response = http.request(Net::HTTP::Get.new(uri.request_uri))
+#
+#        if response.code != "200" then
+#            reset_session
+#            redirect_to '/auth/facebook'
+#            return
+#        end
 
         require 'json'
-        friend_list = JSON.parse(response.body)
+        friend_list = JSON.parse(@friend_list)
         @friends = Array.new
         friend_list['data'].each do |f|
             res = User.where('uid = :uid', :uid => f['id'])
